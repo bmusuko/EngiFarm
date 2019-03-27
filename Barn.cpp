74ab32164fa78c81b2ab71c74880a17a1f1e29d7
bmusuko <bram.musuko@gmail.com>
2019-03-27 21:45:42 +0700

    Land semua

---
+++ Barn.cpp
@@ -0,0 +1,3 @@
+#include "Barn.hpp"
+
+Barn::Barn(int X, int Y, bool grass, bool object):Land(X,Y,grass,object,false,false,true){}
--- Land.hpp
+++ Land.hpp
@@ -6,17 +6,21 @@ class Land: public Cell{
     protected:
         bool isObjectExist; //Boolean untuk mengecek apakah ada player atau objek lainnya pada petak tersebut
         bool isGrassExist;  //Boolean untuk mengecek apakah ada rumput atau tidak
-        int landType;   //1 coop, 2 grassland, 3 barn
+        bool coop;   //1 coop, 2 grassland, 3 barn
+        bool grassland;
+        bool barn;
     public:
         //Konstruktor
-        Land(int X, int Y, bool grass, bool object, int type);
-
+        Land(int X, int Y, bool grass, bool object, bool coop,bool grassland,bool barn);
         //Setter & Getter
         bool getIsGrassExist() const;
         bool getIsObjectExist () const;
         int getLandType() const;
         void setIsGrassExist(bool grass);
         void setIsObjectExist(bool object);
+        void isCoop() const;
+        void isGrassLand() const;
+        void isBarn() const;
 
 };
 
