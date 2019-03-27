2a3383649698e42e4d2dfe8818e80fb1783b30d2
Valentino Simamora <valent.14021999@gmail.com>
2019-03-27 20:30:30 +0700

    Update

--- Product.cpp
+++ Product.cpp
@@ -1,3 +1,15 @@
 #include "Product.hpp"
 using namespace std;
 
+Product::Product(int harga) {
+    this->harga = harga;
+}
+
+int Product::getHarga() const {
+    return this->harga;
+}
+
+void Product::setHarga(int h) {
+    this->harga = h;
+}
+
